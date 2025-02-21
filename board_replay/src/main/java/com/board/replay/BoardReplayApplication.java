9755a9ad3940cd1f09185a6cbc4e1a010c6b65be
shyunp13 <shyunp13@gmail.com>
2025-02-22 00:31:11 +0900

    서비스, 테스트코드 작성

--- board_replay/src/main/java/com/board/replay/BoardReplayApplication.java
+++ board_replay/src/main/java/com/board/replay/BoardReplayApplication.java
@@ -2,7 +2,9 @@ package com.board.replay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing//jpa auditing기능 활성화
 @SpringBootApplication
 public class BoardReplayApplication {
 
--- board_replay/src/main/java/com/board/replay/service/BoardService.java
+++ board_replay/src/main/java/com/board/replay/service/BoardService.java
@@ -1,5 +1,42 @@
 package com.board.replay.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.board.replay.dto.board.BoardRequestDto;
+import com.board.replay.dto.board.BoardResponseDto;
+import com.board.replay.entity.board.BoardRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
 public class BoardService {
 	//게시판@Service
+	
+	private final BoardRepository boardRepository;
+	
+	
+	@Transactional
+	public Long save(BoardRequestDto boardSaveDto) {
+		return boardRepository.save(boardSaveDto.toEntity()).getId();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<BoardResponseDto>findAll(){
+		return boardRepository.findAll().stream().map(BoardResponseDto::new).collect(Collectors.toList());
+	}
+	
+	public BoardResponseDto findById(Long id) {
+		return new BoardResponseDto(boardRepository.findById(id).get());
+	}
+	
+	public int updateBoard(BoardRequestDto boardRequestDto) {
+		return boardRepository.updateBoard(boardRequestDto);
+	}
+	
+	public void deleteById(Long id) {
+		boardRepository.deleteById(id);
+	}
+	
 }
