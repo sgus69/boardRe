c79cabbb6004558f484ab86ceeb4c3f5ae25137e
sgus <sweetpea13@naver.com>
2025-03-31 22:26:35 +0900

    파일 컨트롤러 작성중

--- board_replay/src/main/java/com/board/replay/web/BoardFileController.java
+++ board_replay/src/main/java/com/board/replay/web/BoardFileController.java
@@ -1,13 +1,47 @@
 package com.board.replay.web;
 
-import org.springframework.stereotype.Controller;
+import java.io.File;
+import java.io.FileNotFoundException;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.board.replay.dto.board.file.BoardFileResponseDto;
 import com.board.replay.service.BoardFileService;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardFileController {
-	private final BoardFileService fileService;
+	private final BoardFileService boardFileService;
+	
+	@GetMapping("/file/download")
+	public void downloadFile(@RequestParam()Long id, HttpServletResponse response)throws Exception{
+		try {
+			//파일정보를 조회한다.
+			BoardFileResponseDto fileInfo = boardFileService.findById(id);
+			
+			if(fileInfo == null)throw new FileNotFoundException("Empty FileData.");
+			
+			//경로와 파일명으로 파일 객체를 생성한다.
+			File dFile = new File(fileInfo.getFilePath(), fileInfo.getSaveFileName());
+			
+			//파일 길이를 가져온다.
+			int fSize = (int)dFile.length();
+			
+			//파일이 존재하면
+			if(fSize > 0) {
+				//파일명을 URLencoder 하여 attachment, Content - Disposition Header로 설정
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+	
+	
 }
