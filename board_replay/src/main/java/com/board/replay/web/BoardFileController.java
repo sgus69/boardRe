a68099cba3ef51052a5924f83e5bb493bfd4e20f
sgus <sweetpea13@naver.com>
2025-04-01 22:09:47 +0900

    파일컨트롤러 파일 다운로드 삭제 작성완.

--- board_replay/src/main/java/com/board/replay/web/BoardFileController.java
+++ board_replay/src/main/java/com/board/replay/web/BoardFileController.java
@@ -1,12 +1,20 @@
 package com.board.replay.web;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.net.URLEncoder;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.board.replay.dto.board.file.BoardFileRequestDto;
+import com.board.replay.dto.board.file.BoardFileRequestDto;
 import com.board.replay.dto.board.file.BoardFileResponseDto;
 import com.board.replay.service.BoardFileService;
 
@@ -35,13 +43,85 @@ public class BoardFileController {
 			//파일이 존재하면
 			if(fSize > 0) {
 				//파일명을 URLencoder 하여 attachment, Content - Disposition Header로 설정
+				String encoderFilename = "attachment; filename*=" + 
+										"UTF-8"+"''"+ URLEncoder.encode(fileInfo.getOrigFileName(), "UTF-8");
+				//ContentType 설정
+				response.setContentType("application/octet-stream; charset=utf-8");
+				
+				//Header 설정
+				response.setHeader("Content-Disposition", encoderFilename);
+				
+				//ContentLength설정
+				response.setContentLengthLong(fSize);
+				
+				BufferedInputStream in = null;
+				BufferedOutputStream out = null;
+				
+				/*BufferedInputStream
+				 * 
+				 java.io의 가장 기본 파일 입출력 클래스
+				 입력 스트림(통로)을 생성해줌
+				 사용법은 간단하지만, 버퍼를 사용하지 않기 때문에 느림
+				 속도 문제를 해결하기 위해 버퍼를 사용하는 다른 클래스와
+				 같이 쓰는 경우가 많음
+				*/
+				in = new BufferedInputStream(new FileInputStream(dFile));
+				
+				/* BufferedOutputStream
+				 *
+				 java.io의 가장 기본이 되는 파일 입출력 클래스
+				 출력 스트림(통로)을 생성해줌
+				 사용법은 간단하지만, 버퍼를 사용하지 않기 때문에 느림
+				 속도 문제를 해결하기 위해 버퍼를 사용하는 다른 클래스와
+				 같이 쓰는 경우가 많음 
+				 */
+				out = new BufferedOutputStream(response.getOutputStream()); 
+				
+				try {
+					byte[] buffer = new byte[4096];
+					int bytesRead = 0;
+					
+					/*
+					 모두 현재 파일 포인터 위치를 기준으로 함(파일 포인터 앞의 내용은 없는 것처럼 작동)
+					 int read(): 1byte씩 내용을 읽어 정수로 반환
+					 int read(byte[] b): 파일 내용을 한번에 모두 읽어서 배열에 저장
+					 int read(byte[] b, int off, int len) : 'len'길이만큼만 읽어서 배열의 'off'번째 위치부터 저장
+					*/
+					
+					while((bytesRead = in.read(buffer)) != -1) {
+						out.write(buffer, 0, bytesRead);
+					}
+					
+					//버퍼에 남은 내용이 있다면, 모두 파일에 출력
+					out.flush();  
+				}finally {
+					/*
+					 현재 열려있는 in, out 스트림을 닫음
+					 메모리 누수를 방지하고 다른 곳에서 리소스 사용이 가능하게 만듬
+					 */
+					in.close();
+					out.close();
+				}
+				
+			}else {
+				throw new FileNotFoundException("Empty FileData.");
 			}
 			
 		} catch (Exception e) {
-			// TODO: handle exception
+			throw new Exception(e.getMessage());
 		}
-		
+	
 	}
 	
-	
+	@PostMapping("/file/delete.ajax")
+	public String updateDeleteYn(Model model, BoardFileRequestDto boardFileRequestDto) throws Exception{
+		
+		try {
+			model.addAttribute("result", boardFileService.updateDeleteYn(boardFileRequestDto.getIdArr()));
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+		
+		return "jsonView";
+	}
 }
