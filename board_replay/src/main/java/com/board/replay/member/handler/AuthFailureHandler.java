53f8e30293b7706b1119ba3eb0ca98e374e9d682
sgus <sweetpea13@naver.com>
2025-07-21 11:03:07 +0900

    로그인 성공실패시 핸들러 작성

--- board_replay/src/main/java/com/board/replay/member/handler/AuthFailureHandler.java
+++ board_replay/src/main/java/com/board/replay/member/handler/AuthFailureHandler.java
@@ -1,13 +1,15 @@
 package com.board.replay.member.handler;
 
-import javax.naming.AuthenticationException;
+import java.io.IOException;
 
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.CredentialsExpiredException;
 import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -22,11 +24,14 @@ public class AuthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 		//exception 관련 메세지 처리
 		if (exception instanceof DisabledException) {
         	msg = "DisabledException account";
+        	//계정 비활성화
         } else if(exception instanceof CredentialsExpiredException) {
         	msg = "CredentialsExpiredException account";
+        	//비밀번호 유효기간 만료
         } else if(exception instanceof BadCredentialsException ) {
         	msg = "BadCredentialsException account";
-        }
+        	//잘못된 비밀번호나 이메일 입력
+        }	
 		setDefaultFailureUrl("/login?error=true&exception=" + msg);
 		
 	    super.onAuthenticationFailure(request, response, exception);
--- board_replay/src/main/java/com/board/replay/member/handler/AuthSuccessHandler.java
+++ board_replay/src/main/java/com/board/replay/member/handler/AuthSuccessHandler.java
@@ -1,5 +1,30 @@
 package com.board.replay.member.handler;
 
-public class AuthSuccessHandler {
+import java.io.IOException;
+import java.time.LocalDateTime;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import com.board.replay.entity.board.member.MemberRepository;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class AuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+	
+	private final MemberRepository memberRepository = null;
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)throws IOException, ServletException {
+		
+		memberRepository.updateMemberLastLogin(authentication.getName(), LocalDateTime.now());
+		setDefaultTargetUrl("/board/list");
+		
+		super.onAuthenticationSuccess(request, response, authentication);
+	}
+	
 }
